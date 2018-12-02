d2e76a6050ab1dced3797121f36845d74eea05e4
Adrian Guillot <adrian.guillot@gmail.com>
2018-12-02 16:10:25 +0100

    day01 part 2

--- day01/src/main.cpp
+++ day01/src/main.cpp
@@ -2,6 +2,34 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <set>
+
+std::set<int> visited;
+bool repeat_found = false;
+int repeated_frequency;
+
+int calculate_frequencies(std::vector<std::string> lines, int start = 0){
+    int sum = start;
+
+    for(auto&& line : lines){
+        // Get int from line
+        sum += std::stoi(line);
+
+        // If frequncy is unique, add it to set
+        // else repeated frequency is found
+        if(visited.find(sum) == visited.end()){
+            visited.insert(sum);
+        } else {
+            // Make sure the first oneif kept
+            if(!repeat_found){
+                repeated_frequency = sum;
+            }
+            repeat_found = true;
+        }
+    }
+
+    return sum;
+}
 
 int main(int argc, char** args){
     // Import input file
@@ -15,11 +43,11 @@ int main(int argc, char** args){
     input.close();
 
     // Process lines
-    int sum = 0;
-    for(auto&& line : lines)
-    {
-        int line_value = std::stoi(line);
-        sum += line_value;
-    }
+    int sum = calculate_frequencies(lines, 0);
     std::cout << "Part 1 - sum: " << sum << std::endl; 
+    
+    while(!repeat_found){
+        sum = calculate_frequencies(lines, sum);
+    }
+    std::cout << "Part 2 - first repeated frequency: " << repeated_frequency << std::endl;
 }
